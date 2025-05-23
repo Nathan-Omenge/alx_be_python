6808b2d7ef12f8febb26c117564baf9f96a32a29
Nathan Orang'o <nathanorango0@gmail.com>
2025-05-23 10:17:26 -0600

    Add basic_operations.py to repository

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Adding the two numbers
+print("Addition of",number1,"and",number2,"is:", number1 + number2)
+
+# Subtracting the Two numbers
+print("Subtraction of",number1,"from",number2,"is:", number1-number2)
+
+# Multiplying the Two Numbers
+print("Multiplication of",number1,"and", number2, "is:", number1*number2)
+
