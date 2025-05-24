048c6872ed027bb97f4338a146c410fa36348f52
Nathan Orang'o <nathanorango0@gmail.com>
2025-05-24 07:58:15 -0600

    Add rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Dimensions of the Rectangle
+length = 10
+width = 5
+
+# Calculating the Area of the Rectangle
+area = length * width
+
+# Displaying Results
+print("The area of ther rectangle is:", area)
