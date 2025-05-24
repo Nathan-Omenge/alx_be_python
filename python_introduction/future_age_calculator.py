48af38f2829c7180032db4a3eaddb5b221d44d30
Nathan Orang'o <nathanorango0@gmail.com>
2025-05-24 10:33:37 -0600

    Updated to fix errors

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 # Prompting the User for their currrent age
-current_age = int(input("How old are you?: "))
+current_age = int(input("How old are you? "))
 
 # Calculating the  User's Age in 2050
 future_age = current_age + 27
