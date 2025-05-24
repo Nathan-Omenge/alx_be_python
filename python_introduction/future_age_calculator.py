9fba48f190a658ce3fcdeb95ff6b2dd1de5ab4d6
Nathan Orang'o <nathanorango0@gmail.com>
2025-05-24 10:14:57 -0600

    Fixed errors in syntax

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,8 @@
 # Prompting the User for their currrent age
-current_age = int(input("How Old Are You?: "))
+current_age = int(input("How old are you?: "))
 
 # Calculating the  User's Age in 2050
 future_age = current_age + 27
 
 # Displaying the Results
-print(f"In 2050, you will be {future_age} years old. ")
+print(f"In 2050, you will be {future_age} years old. ") 
